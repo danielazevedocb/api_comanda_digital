13d8596a3129f725bdac263b3c7214a60b8d0074
danielazevedocb <daniel.santoscb@gmail.com>
2024-04-29 16:33:11 -0300

    .

--- src/main.ts
+++ src/main.ts
@@ -11,21 +11,12 @@ async function bootstrap() {
     .setTitle('Comanda Digital - API')
     .setDescription('API de autenticação')
     .setVersion('1.0')
-    .addBearerAuth(
-      {
-        type: 'http',
-        scheme: 'bearer',
-        bearerFormat: 'JWT',
-      },
-      'JWT',
-    )
+    .addTag('tag')
+
     .build();
 
-    const document = SwaggerModule.createDocument(app, config);
-    SwaggerModule.setup('docs', app, document, {
-      customCssUrl: 'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.1.3/swagger-ui.css',
-      customJs: 'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.1.3/swagger-ui-bundle.js'
-    });
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document, {});
 
   // Escuta na porta disponível pelo ambiente
   await app.listen(3000);
