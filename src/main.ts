9810ea53ecc663d02815c454f0c9c5546c0f8a0f
danielazevedocb <daniel.santoscb@gmail.com>
2024-04-29 17:41:44 -0300

    .

--- src/main.ts
+++ src/main.ts
@@ -5,23 +5,39 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe());
+
+  // Configuração CORS - Ajuste conforme necessário
+  app.enableCors({
+    
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    preflightContinue: false,
+    optionsSuccessStatus: 204,
+    credentials: true,
+  });
+
+  app.useGlobalPipes(new ValidationPipe({
+    whitelist: true, // Remove os dados que não estão nos DTOs
+    forbidNonWhitelisted: true, // Lança erro se contiver dados que não estão nos DTOs
+    transform: true, // Transforma o input nos tipos dos DTOs
+  }));
 
   const config = new DocumentBuilder()
     .setTitle('Comanda Digital - API')
     .setDescription('API de autenticação')
     .setVersion('1.0')
-    .addTag('tag')
+    .addBearerAuth( // Configuração para suporte de autenticação JWT
+      { type: 'http', scheme: 'bearer', bearerFormat: 'JWT' },
+      'access-token',
+    )
     .build();
 
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('docs', app, document, {});
+  SwaggerModule.setup('docs', app, document);
 
-  // Use a porta fornecida pelo ambiente ou padrão para 3000
+  // Uso de variável de ambiente para a porta
   const port = process.env.PORT || 3000;
-
-  // Escuta na porta fornecida pelo ambiente
   await app.listen(port);
+  console.log(`Application is running on port ${port}`);
 }
 
 bootstrap();
