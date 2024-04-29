a19ed72416e955e4578ebb8ba2afb251c14bd230
danielazevedocb <daniel.santoscb@gmail.com>
2024-04-29 15:32:58 -0300

    feat: correction made to the vercel.json and main ts file

--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,6 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
-import * as express from 'express';
-import { join } from 'path';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -11,14 +9,19 @@ async function bootstrap() {
   
   const config = new DocumentBuilder()
     .setTitle('Comanda digital - API')
-    .setDescription('API  de autenticação')
+    .setDescription('API de autenticação')
     .setVersion('1.0')
-    .addBearerAuth()
+    .addBearerAuth(
+      {
+        type: 'http',
+        scheme: 'bearer',
+        bearerFormat: 'JWT',
+      },
+      'JWT',
+    )
     .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('docs', app, document);
-
-  app.use(express.static(join(__dirname, '..', 'public')));
 
   await app.listen(3000);
 }
