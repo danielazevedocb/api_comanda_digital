72882371a31d0e91400248065a52e2f4e45dc60b
danielazevedocb <daniel.santoscb@gmail.com>
2024-04-29 15:44:41 -0300

    feat: correction made to the file and main ts

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,8 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import * as express from 'express';
+import * as swaggerUI from 'swagger-ui-dist';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -22,6 +24,14 @@ async function bootstrap() {
     .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('docs', app, document);
+
+  // Carregar recursos do Swagger UI via CDN
+  const swaggerIndex = swaggerUI.getAbsoluteFSPath();
+  app.use('/swagger', express.static(swaggerIndex));
+  app.use('/swagger', express.static(swaggerUI.getAbsoluteFSPath('index.html')));
+  app.use('/swagger', express.static(swaggerUI.getAbsoluteFSPath('swagger-ui.css')));
+  app.use('/swagger', express.static(swaggerUI.getAbsoluteFSPath('swagger-ui-bundle.js')));
+  app.use('/swagger', express.static(swaggerUI.getAbsoluteFSPath('swagger-ui-standalone-preset.js')));
 
   await app.listen(3000);
 }
